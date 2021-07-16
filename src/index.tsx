ea8df5a4a8f3912d265cbfeb65b9ed261ba4d837
wangym <wangym0203@aliyun.com>
2021-07-16 14:44:29 +0800

    feat: test prettierrc

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { Button } from "antd";
-import styles from "./index.less";
-
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Button } from 'antd';
+import styles from './index.less';
 
 ReactDOM.render(
   <div className={styles.app}>
     <a>123</a>
-    <Button type="primary">hello webpack !!!123</Button>{" "}
+    <Button type="primary">hello webpack !!!123</Button>{' '}
   </div>,
-  document.getElementById("root")
+  document.getElementById('root'),
 );
