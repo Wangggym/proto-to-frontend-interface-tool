d3bbf44cb3c0ba04fcb9d77ae2273256c5024edd
wangym <wangym0203@aliyun.com>
2021-07-23 10:47:23 +0800

    feat: 功能修改

--- src/page/index.tsx
+++ src/page/index.tsx
@@ -1,8 +1,27 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Input, message, Tabs } from 'antd';
 import { parseProto } from '../pb_to_ts';
 import { CopyToClipboard } from 'react-copy-to-clipboard';
 import styles from './index.less';
+
+const defaultFile = `
+syntax = "proto3";
+option go_package = "ccc/aaa";
+service MyService {
+  // POST /aefw/few/fwe --> 这里是注释
+    rpc MyMethod (MyRequest) returns (MyResponse);
+}
+
+message MyRequest {
+  // 这里是注释
+    string path = 1;
+}
+
+message MyResponse {
+  // 这里是注释
+    int32 status = 1;
+}
+  `;
 
 const { TabPane } = Tabs;
 const { TextArea } = Input;
@@ -10,12 +29,24 @@ const { TextArea } = Input;
 const App: React.FC = () => {
   const [methods, setMethods] = useState<string>();
   const [types, setTypes] = useState<string>();
+  const [inputValue, setInputValue] = useState<string>(defaultFile);
 
   const handleChange = (v: string) => {
-    const { allMethods, allTypes } = parseProto(v);
-    setMethods(allMethods);
-    setTypes(allTypes);
+    setInputValue(v);
   };
+
+  useEffect(() => {
+    if (inputValue) {
+      try {
+        const { allMethods, allTypes } = parseProto(inputValue);
+        setMethods(allMethods);
+        setTypes(allTypes);
+      } catch (error) {
+        console.log(error);
+        message.warn(`格式不正确`);
+      }
+    }
+  }, [inputValue]);
 
   return (
     <div className={styles.warpper}>
@@ -29,6 +60,7 @@ const App: React.FC = () => {
                 <TextArea
                   autoSize={{ minRows: 22 }}
                   onChange={(v) => handleChange(v.target.value)}
+                  value={inputValue}
                 />
               </div>
             </div>
